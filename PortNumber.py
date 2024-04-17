fff2ae4bea4ea4c428d9d38676869becede342cc
Tomtubinas <143620957+Tomtubinas@users.noreply.github.com>
2024-04-17 12:58:19 -0400

    Update PortNumber.py

--- PortNumber.py
+++ PortNumber.py
@@ -1,39 +1,26 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-badPorts = []
+Ports = []
+IPs = []
+Usernames = []
 
 def Add():
-    portAdd = True
-    print("What port number do you want do ban?\nWhen your done enter Next")
-    while portAdd:
-        PortNum = input("Enter Port Number: ")
-        if PortNum.lower() == "next":
-            portAdd = False
-        elif PortNum.isdigit():
-            badPorts.append(PortNum)
-        else:
-            print("Invalid Option. Please Enter Port Number")
-    print(badPorts)
-
-def Remove():
-    portRemove = True
-    print("Enter the port number you want to remove\nWhen your done enter Next")
-    print(badPorts)
-    while portRemove:
-        PortNum = input("Enter Port Number: ")
-        if PortNum.lower() == "next":
-            portRemove = False
-        elif PortNum.isdigit():
-            badPorts.remove(PortNum)
-        else:
-            print("Invalid Option. Please Enter Port Number")
-    print(badPorts)
-
+    toAdd = int(input("What do you want to add to the list?\n1: Port\n2: IP\n3: Username"))
+    if toAdd == 1:
+        portAdd = int(input("Enter Port number"))
+        Ports.append(portAdd)
+    elif toAdd == 2:
+        Remove()
+    elif toAdd == 3:
+        loop = False
+        print("Exiting the program...")
+    else:
+        print("Invalid option. Please enter 1, 2, or 3.")
 
 loop = True
 while loop:
-    print("What do you want to do\n1) Add Port Number To Be Banned\n2) Unban Port Number")
+    print("What do you want to do\n1: Add Port Number To Be Banned\n2: Unban Port Number")
     num = input()
     if num == "1":
         Add()
