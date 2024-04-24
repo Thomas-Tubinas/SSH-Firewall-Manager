df944fd4220688e92dc1bb39be8bdd8fc1c200d0
Tomtubinas <143620957+Tomtubinas@users.noreply.github.com>
2024-04-24 09:53:46 -0400

    Update PortNumber.py
    Made a access control txt file to save files

--- PortNumber.py
+++ PortNumber.py
@@ -1,6 +1,3 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
 Ports = []
 IPs = []
 Usernames = []
@@ -49,9 +46,40 @@ def Remove():
     else:
         print("Invalid option. Please enter 1, 2, or 3.")
 
+def Save():
+    with open("acl.txt", "w") as file:
+        file.write("Ports:\n")
+        file.write('\n'.join(map(str, Ports)))
+        file.write("\n\nIPs:\n")
+        file.write('\n'.join(IPs))
+        file.write("\n\nUsernames:\n")
+        file.write('\n'.join(Usernames))
+
+def Load():
+    global Ports, IPs, Usernames
+    with open("acl.txt", "r") as file:
+        lines = file.readlines()
+        mode = None
+        for line in file:
+            line = line.strip()
+            if line == "Ports:":
+                mode = "Ports"
+            elif line == "IPs:":
+                mode = "IPs"
+            elif line == "Usernames:":
+                mode = "Usernames"
+            elif mode == "Ports" and line:
+                Ports.append(int(line))
+            elif mode == "IPs" and line:
+                IPs.append(line)
+            elif mode == "Usernames" and line:
+                Usernames.append(line)
+
+
+Load()
 loop = True
 while loop:
-    print("What do you want to do\n1: Add to Access List\n2: Remove from Access List\n3: Check activity")
+    print("What do you want to do\n1: Add to Access List\n2: Remove from Access List\n3: Check activity\n4: Save and Exit")
     num = input()
     if num == "1":
         Add()
@@ -60,5 +88,8 @@ while loop:
     elif num == "3":
         #report()
         print("Checking")
+    elif num == "4":
+        Save()
+        loop = False
     else:
-        print("Invalid option. Please enter 1, 2, or 3.")
+        print("Invalid option. Please enter 1, 2, 3, or 4")
